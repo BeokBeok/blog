8609b7c2a3966968359b8e7dfc8fea31cb1ae5ca
BeokBeok <kekemusa37@gmail.com>
2022-06-12 12:03:19 +0900

    [생성] 프로젝트

---
+++ components/Footer.tsx
@@ -0,0 +1,124 @@
+import * as React from 'react'
+import { FaRssSquare } from '@react-icons/all-files/fa/FaRssSquare'
+import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
+import { FaZhihu } from '@react-icons/all-files/fa/FaZhihu'
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
+import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
+
+import { useDarkMode } from 'lib/use-dark-mode'
+import * as config from 'lib/config'
+
+import styles from './styles.module.css'
+
+// TODO: merge the data and icons from PageSocial with the social links in Footer
+
+export const FooterImpl: React.FC = () => {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+  const onToggleDarkMode = React.useCallback(
+    (e) => {
+      e.preventDefault()
+      toggleDarkMode()
+    },
+    [toggleDarkMode]
+  )
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>
+        <div style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <a href='https://hits.seeyoufarm.com'>
+            <img
+              style={{ display: 'block', margin: '0px auto' }}
+              src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fdaydev.vercel.app&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false'
+            />
+          </a>
+        </div>
+        <div>Copyright 2022 {config.author} </div>
+      </div>
+
+      <div className={styles.settings}>
+        {hasMounted && (
+          <a
+            className={styles.toggleDarkMode}
+            href='#'
+            role='button'
+            onClick={onToggleDarkMode}
+            title='Toggle dark mode'
+          >
+            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+          </a>
+        )}
+      </div>
+
+      <div className={styles.social}>
+        <a
+          className={styles.rss}
+          href={`https://${config.domain}/feed`}
+          title={`RSS`}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaRssSquare />
+        </a>
+
+        {config.twitter && (
+          <a
+            className={styles.twitter}
+            href={`https://twitter.com/${config.twitter}`}
+            title={`Twitter @${config.twitter}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaTwitter />
+          </a>
+        )}
+
+        {config.zhihu && (
+          <a
+            className={styles.zhihu}
+            href={`https://zhihu.com/people/${config.zhihu}`}
+            title={`Zhihu @${config.zhihu}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaZhihu />
+          </a>
+        )}
+
+        {config.github && (
+          <a
+            className={styles.github}
+            href={`https://github.com/${config.github}`}
+            title={`GitHub @${config.github}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub />
+          </a>
+        )}
+
+        {config.linkedin && (
+          <a
+            className={styles.linkedin}
+            href={`https://www.linkedin.com/in/${config.linkedin}`}
+            title={`LinkedIn ${config.author}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin />
+          </a>
+        )}
+      </div>
+    </footer>
+  )
+}
+
+export const Footer = React.memo(FooterImpl)
