9b4304ce9a846248bf358ce70fc63cc7124780d0
BeokBeok <kekemusa37@gmail.com>
2022-06-12 12:13:07 +0900

    [변경] rootNotionPageId

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '17a33b1890994cbfbe7ef681514d9c23',
+  rootNotionPageId: 'a7924ccde50748a1be2e7bb0c603d753',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
